bc98ef22a5d046d58ce38459f0583913ef60d0fd
Austin Walker <austinsrwalker@gmail.com>
2020-06-08 08:50:46 -0700

    finished with hesitation

--- code-challenges.js
+++ code-challenges.js
@@ -8,6 +8,14 @@ var people = [
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "radio employee" }
 ]
+
+const sentence = (array) => {
+  let peoplesJobs = array.map((value,index) =>{
+    return `${value.name} is a ${value.occupation}`
+  })
+  return peoplesJobs
+}
+console.log(sentence(people));
 // Expected output example: "Ford Prefect is a hitchhiker." "Zaphod Beeblebrox is a president of the galaxy." "Arthus Dent is a radio employee."
 
 
@@ -15,18 +23,34 @@ var people = [
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the remainder of the numbers when divided by 3.
-
+//
 var testingArray1 = [23, "Heyyyy!", 45, -9, 0, "Yo", false]
-// Expected output: [ 2, 0, -0, 0 ]
-var testingArray2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+// // Expected output: [ 2, 0, -0, 0 ]
+ var testingArray2 = [5, "Hola", 43, -34, "greetings", true]
+// // Expected output: [ 2, 1, -1 ]
+//
+const modulo3 = (array) =>{
+  let filteredArray = array.filter((value)=> typeof (value) == 'number')
+  //.filter !== even
+  let finalArray = filteredArray.map((value)=> value % 3)
+
+  return finalArray
+}
+console.log(modulo3(testingArray1));
+console.log(modulo3(testingArray2));
+//
+//
+//
+//
+// // --------------------3) Create a function that takes in two arrays as arguments returns one array with no duplicate values.
+//
+testingArray3 = [3, 7, "hi", 10, 3, "hello", 4, "hi"]
+testingArray4 = [7, "hi", 3, 1, "hi", 4, "hello", 4, 7]
+// // Expected output: [ 3, 7, "hi", 10, "hello", 4, 1 ]
 
 
-
-
-
-// --------------------3) Create a function that takes in two arrays as arguments returns one array with no duplicate values.
-
-var testingArray3 = [3, 7, "hi", 10, 3, "hello", 4, "hi"]
-var testingArray4 = [7, "hi", 3, 1, "hi", 4, "hello", 4, 7]
-// Expected output: [ 3, 7, "hi", 10, "hello", 4, 1 ]
+const Joined = (array, array2) => {
+  const arr3 = array.concat(array2)
+  return [...new Set(arr3)]
+}
+console.log(sortedJoined(testingArray3, testingArray4));
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -5,24 +5,25 @@
 
 # app/controller/blog_posts_controller.rb
 
-# 1)
+# 1) The controller for Blog  uses Application contoller as a parent component. This contoller only works for "Blog"
 class BlogPostsController < ApplicationController
   def index
-    # 2)
+    # 2) Allows the "GET" method to retive all json data
     @posts = BlogPost.all
   end
 
   def show
-    # 3)
+    # 3) allows access to a single json entry via its ID
     @post = BlogPost.find(params[:id])
   end
 
-  # 4)
+  # 4) "This is where I'd put my method IF I HAD ONE" Mr. Turner,  FairlyOddParents -- A CREATE method that doesnt save
+  # https://www.youtube.com/watch?v=_Tc0C3fhjt0  -- 11secs
   def new
   end
 
   def create
-    # 5)
+    # 5) Call the CREATE method and save it upon accpetance through validations
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to @post
@@ -36,15 +37,15 @@ class BlogPostsController < ApplicationController
     if @post.delete
       redirect_to blog_posts_path
     else
-      # 6)
+      # 6)Shows that the @post has not been destroyed by displaying it amongst the other posts
       redirect_to blog_post_path(@post)
     end
   end
 
-  # 7)
+  # 7) "internal helper models" private is only accessible within our app and (we) mainly used for setting up strong Parameters
   private
   def blog_post_params
-    # 8)
+    # 8)to create a blog post two arguments of title, and contnet must be passed or post will be rejected/ filled with nil values.
     params.require(:blog_post).permit(:title, :content)
   end
 
@@ -53,8 +54,8 @@ end
 
 # app/models/blog_post.rb
 
-# 9)
+# 9)The Model of BlogPost
 class BlogPost < ApplicationRecord
-  # 10)
+  # 10) Takes the child component of comments and attributes its tables as data to each individual entry of BlogPost
   has_many :comments
 end
